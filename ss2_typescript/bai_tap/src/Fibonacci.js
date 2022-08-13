dd737c4cd2ce2985e2110d4b5c2b320af3ad603a
tranngocdam <tranngocdam12gmail.com>
2022-08-13 09:50:23 +0700

    nop bai tap ngay

--- ss2_typescript/bai_tap/src/Fibonacci.js
+++ ss2_typescript/bai_tap/src/Fibonacci.js
@@ -1,14 +1,14 @@
-function Fibonacci(n) {
-    if (n == 1 || n == 2) {
-        return 1;
+function fibonacci(n) {
+    if (n == 0 || n == 1) {
+        return n;
     }
-    return Fibonacci(n - 1) + Fibonacci(n - 2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 var sum = 0;
 var k = 10;
-console.log("Số Fibonacci thứ 10: " + Fibonacci(k));
-for (var i = 1; i <= k; i++) {
-    console.log("Số Fibonacci thứ " + (i) + ":" + Fibonacci(i));
-    sum += Fibonacci(i);
+console.log("Số Fibonacci thứ 10: " + fibonacci(k));
+for (var i = 0; i < k; i++) {
+    console.log("Số Fibonacci thứ " + (i + 1) + ":" + fibonacci(i));
+    sum += fibonacci(i);
 }
 console.log("Tổng 10 số Fibonacci đầu tiên: " + sum);
