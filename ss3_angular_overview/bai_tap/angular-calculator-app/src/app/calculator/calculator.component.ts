85c563f870ed08b942470dac36ba221305ed78e1
tranngocdam <tranngocdam12gmail.com>
2022-08-15 19:08:25 +0700

    nop bai tap ngay

---
+++ ss3_angular_overview/bai_tap/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+  number1 = 0;
+  number2 = 0;
+  result = 0;
+  calculator(operator: string): number {
+    switch (operator) {
+      case '+':
+         return this.result = this.number1 + this.number2;
+         break;
+      case '-':
+         return this.result = this.number1 - this.number2;
+         break;
+      case '*':
+         return this.result = this.number1 * this.number2;
+         break;
+      case '/':
+         return this.result = this.number1 / this.number2;
+         break;
+    }
+  }
+}
